ec9daf026d794009d9e862cbcda5b54cbdb351c1
MrXyfir <mr@xyfir.com>
2019-04-20 12:50:59 -0700

    Update App container

--- web/components/App.tsx
+++ web/components/App.tsx
@@ -1,20 +1,56 @@
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import { MuiThemeProvider, CssBaseline } from '@material-ui/core';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { SnackbarProvider } from 'notistack';
+import { Navigation } from 'components/Navigation';
+import { Import } from 'components/Import';
 import * as React from 'react';
 import { theme } from 'constants/theme';
+import {
+  MuiThemeProvider,
+  createStyles,
+  CssBaseline,
+  WithStyles,
+  withStyles,
+  Button,
+  Theme
+} from '@material-ui/core';
 
-export class App extends React.Component {
-  render() {
-    return (
-      <MuiThemeProvider theme={theme}>
-        <CssBaseline />
-        <BrowserRouter>
-          <Switch>
-            <Route path="/info" render={() => <div />} />
-            <Redirect exact from="/" to="/info" />
-          </Switch>
-        </BrowserRouter>
-      </MuiThemeProvider>
-    );
-  }
-}
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex'
+    },
+    toolbar: theme.mixins.toolbar,
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing.unit * 3
+    }
+  });
+
+const _App = ({ classes }: WithStyles<typeof styles>) => (
+  <MuiThemeProvider theme={theme}>
+    <CssBaseline />
+    <SnackbarProvider
+      action={[
+        <Button color="primary" size="small">
+          Dismiss
+        </Button>
+      ]}
+      maxSnack={2}
+      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+    >
+      <BrowserRouter>
+        <Switch>
+          <div className={classes.root}>
+            <Navigation />
+            <main className={classes.content}>
+              <div className={classes.toolbar} />
+              <Route path="/import" render={() => <Import />} />
+            </main>
+          </div>
+        </Switch>
+      </BrowserRouter>
+    </SnackbarProvider>
+  </MuiThemeProvider>
+);
+
+export const App = withStyles(styles)(_App);
