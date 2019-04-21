c30716286394f716c3baac23d7708269462a6e15
MrXyfir <mr@xyfir.com>
2019-04-20 19:51:01 -0700

    Update theme

--- web/constants/theme.ts
+++ web/constants/theme.ts
@@ -3,7 +3,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 export const theme = createMuiTheme({
   palette: {
     type: localStorage.theme,
-    primary: { main: '#212121' },
-    secondary: { main: '#fafafa' }
+    primary: { main: '#1976d2' },
+    secondary: { main: '#64dd17' }
   }
 });
