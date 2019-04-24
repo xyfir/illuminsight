8075b311f5afd6904f042b270b1eb302327ce0f7
MrXyfir <mr@xyfir.com>
2019-04-23 20:11:51 -0700

    Add styles

--- web/components/Cover.tsx
+++ web/components/Cover.tsx
@@ -1,9 +1,22 @@
-import { createStyles, WithStyles, withStyles } from '@material-ui/core';
+import { createStyles, WithStyles, withStyles, Theme } from '@material-ui/core';
 import { CropSquare as SquareIcon } from '@material-ui/icons';
 import * as localForage from 'localforage';
 import * as React from 'react';
 
-const styles = createStyles({});
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      width: '5em',
+      height: '5em',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: theme.palette.grey[500]
+    },
+    icon: {
+      fontSize: '5em'
+    }
+  });
 
 function _Cover({ classes, id }: WithStyles<typeof styles> & { id: number }) {
   const [url, setUrl] = React.useState('');
@@ -26,7 +39,11 @@ function _Cover({ classes, id }: WithStyles<typeof styles> & { id: number }) {
     };
   }, [id]);
 
-  return <div>{url ? <img src={url} /> : <SquareIcon />}</div>;
+  return (
+    <div className={classes.root}>
+      {url ? <img src={url} /> : <SquareIcon className={classes.icon} />}
+    </div>
+  );
 }
 
 export const Cover = withStyles(styles)(_Cover);
