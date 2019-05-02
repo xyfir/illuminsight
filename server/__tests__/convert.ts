e3e91f08fe7418dd9722a5cad0bb29692fcf94ed
MrXyfir <mr@xyfir.com>
2019-05-01 18:18:15 -0700

    Add convert({text}) test

--- server/__tests__/convert.ts
+++ server/__tests__/convert.ts
@@ -7,15 +7,17 @@ import { Insightful } from 'types/insightful';
 import { countWords } from 'lib/count-words';
 import { nodeToAst } from 'lib/node-to-ast';
 import { convert } from 'lib/convert';
+import { Extract } from 'unzipper';
 import { resolve } from 'path';
 import { pandoc } from 'lib/pandoc';
 import { JSDOM } from 'jsdom';
 import {
+  constants as FS,
   ensureDir,
-  writeFile,
   emptyDir,
   readJSON,
   readFile,
+  access,
   copy
 } from 'fs-extra';
 import 'jest-extended';
@@ -25,6 +27,8 @@ const { SERVER_DIRECTORY, TEMP_DIR } = process.enve;
 
 // Set paths used in tests
 const convertDirectory = resolve(TEMP_DIR, 'convert-test');
+const astpubDirectory = resolve(convertDirectory, 'astpub');
+const astpubMetaFile = resolve(astpubDirectory, 'meta.json');
 const loremHtmlFile = resolve(SERVER_DIRECTORY, 'res/lorem.html');
 const loremAstFile = resolve(SERVER_DIRECTORY, 'res/lorem.ast.json');
 const loremMdFile = resolve(convertDirectory, 'lorem.md');
@@ -78,4 +82,42 @@ test('pandoc()', async () => {
   expect(markdown).toMatch(/^# Lorem Ipsum/m);
   expect(markdown).toMatch(/^#### "Neque porro/m);
   expect(markdown).toMatch(/^Lorem ipsum dolor/m);
+});
+
+test('convert({text})', async () => {
+  // Convert content to astpub format then extract
+  const readStream = await convert({ text: 'Hello World' });
+  await new Promise(resolve =>
+    readStream.pipe(Extract({ path: astpubDirectory }).on('close', resolve))
+  );
+
+  // Validate meta.json
+  const entity: Insightful.Entity = await readJSON(astpubMetaFile);
+  const _entity: Insightful.Entity = {
+    authors: 'Unknown',
+    bookmark: { element: 0, line: 0, section: 0, width: 0 },
+    cover: 'images/cover_image.jpg',
+    id: entity.id,
+    name: entity.name,
+    spine: ['titlepage.xhtml', 'index-1.html'],
+    starred: false,
+    tags: [],
+    version: 1,
+    words: '2'
+  };
+  expect(entity).toMatchObject(_entity);
+
+  // Validate cover
+  await access(resolve(astpubDirectory, 'images/cover_image.jpg'), FS.F_OK);
+
+  // Validate AST
+  const ast: Array<Insightful.AST | string> = await readJSON(
+    resolve(astpubDirectory, 'index-1.html.json')
+  );
+  const _ast: Array<Insightful.AST | string> = [
+    '\n',
+    { c: ['Hello World'], n: 'p' },
+    '\n'
+  ];
+  expect(ast).toMatchObject(_ast);
 });
