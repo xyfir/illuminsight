fb976ff0061df2549eafc52892b05c2a1c8589e2
MrXyfir <mr@xyfir.com>
2019-05-08 12:03:45 -0700

    Clean up code

--- web/__tests__/components/AST.spec.tsx
+++ web/__tests__/components/AST.spec.tsx
@@ -94,42 +94,59 @@ test('<AST>', async () => {
       ))}
     </div>
   );
-
   let el: HTMLElement;
 
-  // Expect properly rendered AST
+  // h1
   el = getByText('Heading 1');
   expect(el.tagName).toBe('H1');
+
+  // p
   el = getByText('This is a paragraph', { exact: false });
   expect(el.tagName).toBe('P');
+
+  // p > a
   el = getByText('with a link');
   expect(el.tagName).toBe('A');
   expect((el as HTMLAnchorElement).href).toBe('https://example.com/');
+
+  // p > em
   expect((el.parentElement as HTMLParagraphElement).tagName).toBe('P');
   el = getByText('emphasised text');
   expect(el.tagName).toBe('EM');
+
+  // p > em > strong
   expect((el.parentElement as HTMLParagraphElement).tagName).toBe('P');
   el = getByText('strongly');
   expect(el.tagName).toBe('STRONG');
+
+  // p > code
   el = getByText('inline code');
   expect(el.tagName).toBe('CODE');
   expect((el.parentElement as HTMLParagraphElement).tagName).toBe('P');
+
+  // hr
   expect(container.querySelector('hr')).not.toBeNull();
+
+  // img
   el = getByAltText('A picture of ...');
   expect(el.tagName).toBe('IMG');
   expect((el as HTMLImageElement).src).toBe('https://example.com/img.png');
+
+  // ul > li
   el = getByText('UL item #1');
   expect(el.tagName).toBe('LI');
   expect((el.parentElement as HTMLUListElement).tagName).toBe('UL');
   el = getByText('UL item #2');
   expect(el.tagName).toBe('LI');
+
+  // ol > li
   el = getByText('OL item #1');
   expect(el.tagName).toBe('LI');
   expect((el.parentElement as HTMLOListElement).tagName).toBe('OL');
   el = getByText('OL item #2');
   expect(el.tagName).toBe('LI');
-  el = getByText('OL item #2');
-  expect(el.tagName).toBe('LI');
+
+  // pre > code
   el = getByText('code block line 1', { exact: false });
   expect(el.tagName).toBe('CODE');
   expect((el.parentElement as HTMLPreElement).tagName).toBe('PRE');
