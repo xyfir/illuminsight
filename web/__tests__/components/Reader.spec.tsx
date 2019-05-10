871b8c4acb43b22df5011d6d7f4c066462672795
MrXyfir <mr@xyfir.com>
2019-05-10 12:27:01 -0700

    Test section navigation

--- web/__tests__/components/Reader.spec.tsx
+++ web/__tests__/components/Reader.spec.tsx
@@ -1,11 +1,16 @@
+import { alternateTestAST, testEntity, testAST } from 'lib/test/objects';
 import { MemoryRouter, Switch, Route } from 'react-router-dom';
-import { waitForDomChange, render } from 'react-testing-library';
-import { testEntity, testAST } from 'lib/test/objects';
 import { SnackbarProvider } from 'notistack';
 import * as localForage from 'localforage';
 import { Reader } from 'components/Reader';
 import * as React from 'react';
 import * as JSZip from 'jszip';
+import {
+  waitForDomChange,
+  fireEvent,
+  render,
+  wait
+} from 'react-testing-library';
 
 test('<Reader>', async () => {
   // Mock localForage and URL
@@ -38,10 +43,10 @@ test('<Reader>', async () => {
 
   // Mock creating image blob url
   const blobUrl = 'blob:/d81d5be1';
-  mockCreateObjectURL.mockReturnValueOnce(blobUrl);
+  mockCreateObjectURL.mockReturnValue(blobUrl);
 
   // Render <Reader>
-  const { getByAltText, unmount } = render(
+  const { getByAltText, getAllByText, getByText } = render(
     <SnackbarProvider>
       <MemoryRouter initialEntries={['/read/1556915133437']}>
         <Switch>
@@ -51,31 +56,28 @@ test('<Reader>', async () => {
     </SnackbarProvider>
   );
 
-  // Await DOM change
-  await waitForDomChange();
-
   // Validate mock loading file from localForage
-  expect(mockGetItem).toHaveBeenCalledTimes(1);
+  await wait(() => expect(mockGetItem).toHaveBeenCalledTimes(1));
   expect(mockGetItem).toHaveBeenCalledWith('entity-1556915133437');
 
   // Validate mock loading blob into JSZip
-  expect(mockLoadAsync).toHaveBeenCalledTimes(1);
+  await wait(() => expect(mockLoadAsync).toHaveBeenCalledTimes(1));
   expect(mockLoadAsync).toHaveBeenCalledWith(fileBlob);
 
   // Validate JSZip mocks
-  expect(mockFile).toHaveBeenCalledTimes(3);
-  expect(mockAsync).toHaveBeenCalledTimes(3);
+  await wait(() => expect(mockFile).toHaveBeenCalledTimes(3));
+  await wait(() => expect(mockAsync).toHaveBeenCalledTimes(3));
 
   // Validate mock loading meta.json from zip
   expect(mockFile.mock.calls[0][0]).toBe('meta.json');
   expect(mockAsync.mock.calls[0][0]).toBe('text');
 
   // Validate mock loading AST from zip
-  expect(mockFile.mock.calls[1][0]).toBe('sections/1.json');
+  expect(mockFile.mock.calls[1][0]).toBe('sections/0.json');
   expect(mockAsync.mock.calls[1][0]).toBe('text');
 
   // Validate mock loading image from zip
-  expect(mockFile.mock.calls[2][0]).toBe('images/1.png');
+  expect(mockFile.mock.calls[2][0]).toBe('images/0.png');
   expect(mockAsync.mock.calls[2][0]).toBe('blob');
 
   // Validate mock creating image blob url
@@ -84,12 +86,61 @@ test('<Reader>', async () => {
 
   // Validate image rendered with blob url
   // An in-depth test of AST rendering is done elsewhere in <AST>'s spec
-  const el = getByAltText('A picture of ...');
+  let el = getByAltText('A picture of ...');
   expect(el.tagName).toBe('IMG');
   expect((el as HTMLImageElement).src).toBe(blobUrl);
 
-  // Validate image blob url was revoked after unmount
+  // Validate a following section exists, while a previous does not
+  expect(getAllByText('Next Section')).toBeArrayOfSize(2);
+  expect(() => getAllByText('Previous Section')).toThrow();
+
+  // Validate image urls have not yet been revoked
   expect(mockRevokeObjectURL).toHaveBeenCalledTimes(0);
-  unmount();
+
+  // Mock loading AST from zip
+  mockAsync.mockReturnValueOnce(JSON.stringify(alternateTestAST));
+
+  // Go to next section
+  fireEvent.click(getAllByText('Next Section')[0]);
+  await waitForDomChange();
+
+  // Validate image urls have been revoked
   expect(mockRevokeObjectURL).toHaveBeenCalledWith(blobUrl);
+
+  // Validate next/previous section exists
+  expect(getAllByText('Next Section')).toBeArrayOfSize(2);
+  expect(getAllByText('Previous Section')).toBeArrayOfSize(2);
+
+  // Validate content has changed
+  el = getByText('Lorem Ipsum ...');
+  expect(el.tagName).toBe('H1');
+
+  // Mock loading AST from zip
+  mockAsync.mockReturnValueOnce(JSON.stringify(testAST));
+
+  // Mock loading image from zip
+  mockAsync.mockReturnValueOnce(imgBlob);
+
+  // Go to next section
+  fireEvent.click(getAllByText('Next Section')[0]);
+  await waitForDomChange();
+
+  // Validate previous section exists, next does not
+  expect(getAllByText('Previous Section')).toBeArrayOfSize(2);
+  expect(() => getAllByText('Next Section')).toThrow();
+
+  // Validate content has changed
+  el = getByText('Heading 1');
+  expect(el.tagName).toBe('H1');
+
+  // Mock loading AST from zip
+  mockAsync.mockReturnValueOnce(JSON.stringify(alternateTestAST));
+
+  // Go to previous section
+  fireEvent.click(getAllByText('Previous Section')[0]);
+  await waitForDomChange();
+
+  // Validate content has changed
+  el = getByText('Lorem Ipsum ...');
+  expect(el.tagName).toBe('H1');
 });
