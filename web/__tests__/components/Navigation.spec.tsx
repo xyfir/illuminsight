f776ecf3b17c2ba971918f59f3e1c97a06fbbda8
MrXyfir <mr@xyfir.com>
2019-05-22 14:25:26 -0700

    Add and improve tests

--- web/__tests__/components/Navigation.spec.tsx
+++ web/__tests__/components/Navigation.spec.tsx
@@ -1,13 +1,28 @@
-import { StaticRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import { Navigation } from 'components/Navigation';
 import { render } from 'react-testing-library';
 import * as React from 'react';
 
-test('<Navigation>', () => {
-  const { asFragment } = render(
-    <StaticRouter>
+test('<Navigation> /', () => {
+  const { getByTitle } = render(
+    <MemoryRouter>
       <Navigation />
-    </StaticRouter>
+    </MemoryRouter>
   );
-  expect(asFragment()).toMatchSnapshot();
+  getByTitle('Toggle light/dark theme');
+  getByTitle('Import content into library');
+  getByTitle('Search items in library');
+  expect(() => getByTitle('Edit metadata')).toThrow();
+});
+
+test('<Navigation> /read/123', () => {
+  const { getByTitle } = render(
+    <MemoryRouter initialEntries={['/read/1234567890123']}>
+      <Navigation />
+    </MemoryRouter>
+  );
+  getByTitle('Toggle light/dark theme');
+  getByTitle('Import content into library');
+  getByTitle('Search items in library');
+  getByTitle('Edit metadata');
 });
