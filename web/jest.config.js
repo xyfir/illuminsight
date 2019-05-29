03f7c5e419a9b478e0495b0b3833f8a1723c26f6
MrXyfir <mr@xyfir.com>
2019-05-28 18:54:25 -0700

    Update and rename lib/test/environment

--- web/jest.config.js
+++ web/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   setupFilesAfterEnv: [
     'react-testing-library/cleanup-after-each',
-    '<rootDir>/lib/test/environment.ts'
+    '<rootDir>/lib/test/setup.ts'
   ],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
--- web/lib/test/environment.ts
+++
@@ -1,4 +0,0 @@
-import { config } from 'dotenv';
-config();
-import 'enve';
-import 'jest-extended';
