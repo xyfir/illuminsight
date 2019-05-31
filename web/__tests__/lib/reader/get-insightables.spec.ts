170d1ac4cde0f043242d478a504be2bfc83aa958
MrXyfir <mr@xyfir.com>
2019-05-30 19:22:54 -0700

    Add getInsightables()

---
+++ web/__tests__/lib/reader/get-insightables.spec.ts
@@ -0,0 +1,16 @@
+import { getInsightables } from 'lib/reader/get-insightables';
+
+test('getInsightables()', () => {
+  const insightables = getInsightables(
+    'What is so special about Insightful? The second largest city in California is San Diego. In July of 1958, NASA was created under President Eisenhower.',
+    'en'
+  );
+  expect(insightables).toMatchObject([
+    'Insightful',
+    'California',
+    'San Diego',
+    'July',
+    'NASA',
+    'President Eisenhower'
+  ]);
+});
