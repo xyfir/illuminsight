f1b7268d30f0980c8b0e64b01e80b61ca0ddd2d6
MrXyfir <mr@xyfir.com>
2019-06-04 11:47:54 -0700

    Improve test

--- web/__tests__/lib/reader/get-insights.spec.ts
+++ web/__tests__/lib/reader/get-insights.spec.ts
@@ -1,19 +1,30 @@
 import { getInsights } from 'lib/reader/get-insights';
+// @ts-ignore
+import * as wtf from 'wtf_wikipedia';
 
 test('getInsights()', async () => {
+  // Mock wtf_wikipedia
+  const mockFetch = ((wtf as any).fetch = jest.fn());
+  mockFetch.mockResolvedValue(null);
+
+  // Get insights
   const insights = await getInsights(
     'What is so special about Insightful? The second largest city in California is San Diego. In July of 1958, NASA was created while President Eisenhower was in office.'
   );
-  expect(insights).toBeArrayOfSize(6);
-  expect(insights[0]).toMatchObject({ text: 'Insightful', wiki: null });
-  expect(insights[1].text).toBe('California');
-  expect(insights[1].wiki.title()).toBe('California');
-  expect(insights[2].text).toBe('San Diego');
-  expect(insights[2].wiki.title()).toBe('San Diego');
-  expect(insights[3].text).toBe('July');
-  expect(insights[3].wiki.title()).toBe('July');
-  expect(insights[4].text).toBe('NASA');
-  expect(insights[4].wiki.title()).toBe('NASA');
-  expect(insights[5].text).toBe('President Eisenhower');
-  expect(insights[5].wiki.title()).toBe('Dwight D. Eisenhower');
+  const items = [
+    'Insightful',
+    'California',
+    'San Diego',
+    'July',
+    'NASA',
+    'President Eisenhower'
+  ];
+
+  // Validate insights
+  expect(mockFetch).toHaveBeenCalledTimes(6);
+  expect(insights).toBeArrayOfSize;
+  for (let i = 0; i < 6; i++) {
+    expect(mockFetch).toHaveBeenNthCalledWith(i + 1, items[i]);
+    expect(insights[i]).toMatchObject({ text: items[i], wiki: null });
+  }
 });
