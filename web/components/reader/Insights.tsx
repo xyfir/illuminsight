ccd06afe78f7b6c325304dd7d69a02d2941d29bf
MrXyfir <mr@xyfir.com>
2019-06-04 20:38:17 -0700

    Add wikipedia article table of contents

--- web/components/reader/Insights.tsx
+++ web/components/reader/Insights.tsx
@@ -4,13 +4,15 @@ import {
   ExpandMore as ExpandMoreIcon,
   ListAlt as StatisticsIcon,
   Search as SearchIcon,
-  Info as InfoIcon
+  Info as InfoIcon,
+  Toc as TOCIcon
 } from '@material-ui/icons';
 import {
   createStyles,
   WithStyles,
   withStyles,
   Typography,
+  IconButton,
   Button,
   Paper,
   Theme,
@@ -19,6 +21,9 @@ import {
 
 const styles = (theme: Theme) =>
   createStyles({
+    tocButton: {
+      marginRight: '0.3em'
+    },
     paper: {
       maxHeight: '60vh',
       overflow: 'auto',
@@ -36,13 +41,17 @@ function _Insights({
 }: { insights: Insightful.Insight[] } & WithStyles<typeof styles>) {
   const [showFullArticle, setShowFullArticle] = React.useState(false);
   const [showInfoBoxes, setShowInfoBoxes] = React.useState(false);
+  const [sectionIndex, setSectionIndex] = React.useState(-1);
   const [showWiki, setShowWiki] = React.useState(-1);
+  const [showTOC, setShowTOC] = React.useState(false);
   const insight = insights[showWiki];
 
   function onClick(insight: Insightful.Insight, i: number) {
     if (insight.wiki) {
       setShowFullArticle(false);
       setShowInfoBoxes(false);
+      setSectionIndex(-1);
+      setShowTOC(false);
       setShowWiki(i);
     } else {
       window.open(
@@ -51,18 +60,62 @@ function _Insights({
     }
   }
 
-  function getArticleHTML() {
-    return (showFullArticle
-      ? insight.wiki!.html()
-      : insight.wiki!.sections()[0].html()
-    ).replace(
+  function getSectionHTML() {
+    let html = '';
+
+    // Get html for main section, full article, or specified section
+    if (sectionIndex > -1) html = insight.wiki!.sections()[sectionIndex].html();
+    else if (showFullArticle) html = insight.wiki!.html();
+    else html = insight.wiki!.sections()[0].html();
+
+    // Get infobox HTML
+    if (showInfoBoxes && !showFullArticle) {
+      html += insight
+        .wiki!.infoboxes()
+        .map((i: any) => i.html())
+        .join('\n');
+    }
+
+    // Update links so they're not local
+    return html.replace(
       /<a class="link" href=".\//g,
       '<a class="link" href="https://en.wikipedia.org/wiki/'
     );
   }
 
+  function WikiTOC({ sections, depth }: { sections: any[]; depth: number }) {
+    return (
+      <ul>
+        {sections
+          .filter(section => section.depth == depth)
+          .map((section: any) => (
+            <li key={section.title()}>
+              <a
+                onClick={() => (
+                  setSectionIndex(
+                    insight
+                      .wiki!.sections()
+                      .findIndex((s: any) => s === section)
+                  ),
+                  setShowTOC(false)
+                )}
+              >
+                {section.title()}
+              </a>
+
+              {/* Nested children */}
+              {section.children().length ? (
+                <WikiTOC sections={section.children()} depth={depth + 1} />
+              ) : null}
+            </li>
+          ))}
+      </ul>
+    );
+  }
+
   return (
     <div>
+      {/* Insight list */}
       {insights.map((insight, i) => (
         <Chip
           key={insight.text}
@@ -73,46 +126,51 @@ function _Insights({
         />
       ))}
 
+      {/* Selected Wikipedia insight */}
       {insight ? (
-        <React.Fragment>
-          <Paper className={classes.paper} elevation={2}>
-            <Typography variant="caption">
-              Source: (English) Wikipedia.org:{' '}
-              <a href={`https://en.wikipedia.org/wiki/${insight.text}`}>
-                {insight.text}
-              </a>
-            </Typography>
+        <Paper className={classes.paper} elevation={2}>
+          {/* Table of Contents */}
+          {insight!.wiki.sections().length > 2 ? (
+            <IconButton
+              className={classes.tocButton}
+              onClick={() => setShowTOC(true)}
+            >
+              <TOCIcon />
+            </IconButton>
+          ) : null}
 
-            <div dangerouslySetInnerHTML={{ __html: getArticleHTML() }} />
+          {/* Attribution */}
+          <Typography variant="caption">
+            Source: (English) Wikipedia.org:{' '}
+            <a href={`https://en.wikipedia.org/wiki/${insight.text}`}>
+              {insight.text}
+            </a>
+          </Typography>
 
-            {showInfoBoxes && !showFullArticle ? (
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: insight
-                    .wiki!.infoboxes()
-                    .map((i: any) => i.html())
-                    .join('\n')
-                }}
-              />
-            ) : null}
+          {showTOC ? (
+            <WikiTOC sections={insight.wiki!.sections().slice(1)} depth={0} />
+          ) : (
+            <React.Fragment>
+              <div dangerouslySetInnerHTML={{ __html: getSectionHTML() }} />
 
-            {showFullArticle ? null : (
-              <Button onClick={() => setShowFullArticle(true)} variant="text">
-                <ExpandMoreIcon />
-                Continue Reading
-              </Button>
-            )}
+              {showFullArticle ? null : (
+                <Button onClick={() => setShowFullArticle(true)} variant="text">
+                  <ExpandMoreIcon />
+                  Continue Reading
+                </Button>
+              )}
 
-            {showInfoBoxes ||
-            showFullArticle ||
-            !insight.wiki!.infoboxes().length ? null : (
-              <Button onClick={() => setShowInfoBoxes(true)} variant="text">
-                <StatisticsIcon />
-                Show Statistics
-              </Button>
-            )}
-          </Paper>
-        </React.Fragment>
+              {showInfoBoxes ||
+              showFullArticle ||
+              !insight.wiki!.infoboxes().length ? null : (
+                <Button onClick={() => setShowInfoBoxes(true)} variant="text">
+                  <StatisticsIcon />
+                  Show Statistics
+                </Button>
+              )}
+            </React.Fragment>
+          )}
+        </Paper>
       ) : null}
     </div>
   );
