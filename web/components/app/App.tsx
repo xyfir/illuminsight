195fc821bda4d1171aa7102e1b2869d2545ef081
MrXyfir <mr@xyfir.com>
2019-06-10 14:13:21 -0700

    Improve MUI styles usage

--- web/components/app/App.tsx
+++ web/components/app/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Redirect, Switch, Route } from 'react-router-dom';
+import { createStyles, CssBaseline, makeStyles } from '@material-ui/core';
 import { SnackbarProvider } from 'notistack';
 import { ThemeProvider } from '@material-ui/styles';
 import { Library } from 'components/library/Library';
@@ -7,15 +8,8 @@ import { Reader } from 'components/reader/Reader';
 import * as React from 'react';
 import { theme } from 'lib/app/theme';
 import { Edit } from 'components/Edit';
-import {
-  createStyles,
-  CssBaseline,
-  WithStyles,
-  withStyles,
-  Theme
-} from '@material-ui/core';
 
-const styles = (theme: Theme) =>
+const useStyles = makeStyles(theme =>
   createStyles({
     toolbar: theme.mixins.toolbar,
     main: {
@@ -24,26 +18,30 @@ const styles = (theme: Theme) =>
       display: 'flex',
       height: '100vh'
     }
-  });
-
-const _App = ({ classes }: WithStyles<typeof styles>) => (
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <SnackbarProvider anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
-      <BrowserRouter>
-        <main className={classes.main}>
-          <div className={classes.toolbar} />
-          <Switch>
-            <Route path="/read/:pubId" component={Reader} />
-            <Route path="/edit/:pubId" component={Edit} />
-            <Route path="/library" component={Library} />
-            <Route path="/import" component={Import} />
-            <Redirect exact from="/" to="/library" />
-          </Switch>
-        </main>
-      </BrowserRouter>
-    </SnackbarProvider>
-  </ThemeProvider>
+  })
 );
 
-export const App = withStyles(styles)(_App);
+export const App = () => {
+  const classes = useStyles();
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <SnackbarProvider
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <BrowserRouter>
+          <main className={classes.main}>
+            <div className={classes.toolbar} />
+            <Switch>
+              <Route path="/read/:pubId" component={Reader} />
+              <Route path="/edit/:pubId" component={Edit} />
+              <Route path="/library" component={Library} />
+              <Route path="/import" component={Import} />
+              <Redirect exact from="/" to="/library" />
+            </Switch>
+          </main>
+        </BrowserRouter>
+      </SnackbarProvider>
+    </ThemeProvider>
+  );
+};
