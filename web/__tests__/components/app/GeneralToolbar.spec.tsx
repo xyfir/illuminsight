2ab8480a98b2bc44c73d6f2ab7e1fb1ad21a81e1
MrXyfir <mr@xyfir.com>
2019-06-10 18:00:03 -0700

    Test install button

--- web/__tests__/components/app/GeneralToolbar.spec.tsx
+++ web/__tests__/components/app/GeneralToolbar.spec.tsx
@@ -3,14 +3,22 @@ import { MemoryRouter } from 'react-router';
 import { render } from '@testing-library/react';
 import * as React from 'react';
 
-test('<GeneralToolbar />', () => {
+test('<GeneralToolbar />', async () => {
+  // Render GeneralToolbar
   const { getByTitle } = render(
     <MemoryRouter>
       <GeneralToolbar />
     </MemoryRouter>,
     { container: document.getElementById('content')! }
   );
+
+  // Test available controls
   getByTitle('Toggle light/dark theme');
   getByTitle('Import content into library');
   getByTitle('Search items in library');
+  expect(() => getByTitle('Install Insightful to home screen')).toThrow();
+
+  // Test beforeinstallprompt event triggering install button
+  window.dispatchEvent(new Event('beforeinstallprompt'));
+  getByTitle('Install Insightful to home screen');
 });
