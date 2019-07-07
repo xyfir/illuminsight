5b89c4ae6568f3620aa54838919d5b8dbf73c339
MrXyfir <mr@xyfir.com>
2019-07-07 14:01:19 -0700

    Mock axios and improve test

--- web/__tests__/lib/reader/generate-insights.spec.ts
+++ web/__tests__/lib/reader/generate-insights.spec.ts
@@ -1,11 +1,18 @@
 import { generateInsights } from 'lib/reader/generate-insights';
 import { defaultRecipe } from 'lib/reader/recipes';
+import { Illuminsight } from 'types/illuminsight';
+import axios from 'axios';
 import wtf from 'wtf_wikipedia';
 
 test('generateInsights()', async () => {
   // Mock wtf_wikipedia
   const mockFetch = ((wtf as any).fetch = jest.fn());
   mockFetch.mockResolvedValue(null);
+
+  // Mock axios (for getting definition)
+  const mockData = {};
+  const mockGet = ((axios as any).get = jest.fn());
+  mockGet.mockResolvedValue({ data: mockData });
 
   // Generate insights from text block
   let insights = await generateInsights(
@@ -25,7 +32,12 @@ test('generateInsights()', async () => {
   expect(mockFetch).toHaveBeenCalledTimes(6);
   expect(insights).toBeArrayOfSize;
   for (let i = 0; i < 6; i++) {
-    expect(insights[i]).toMatchObject({ text: items[i] });
+    const insight: Illuminsight.Insight = {
+      definitions: {},
+      wikis: [],
+      text: items[i]
+    };
+    expect(insights[i]).toMatchObject(insight);
   }
 
   // Generate insights from highlighted text
@@ -36,5 +48,9 @@ test('generateInsights()', async () => {
   expect(mockFetch).toHaveBeenNthCalledWith(7, 'hello world', undefined, {
     wikiUrl: 'https://en.wikipedia.org/w/api.php'
   });
-  expect(insights[0]).toMatchObject({ text: 'hello world' });
+  expect(insights[0]).toMatchObject({
+    definitions: {},
+    wikis: [],
+    text: 'hello world'
+  });
 });
