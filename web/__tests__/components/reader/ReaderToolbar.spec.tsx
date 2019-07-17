38a16491f6c3e9c8ba1cd76056300779c24b825a
MrXyfir <mr@xyfir.com>
2019-07-17 13:19:53 -0700

    Test auto recipe matching

--- web/__tests__/components/reader/ReaderToolbar.spec.tsx
+++ web/__tests__/components/reader/ReaderToolbar.spec.tsx
@@ -1,11 +1,14 @@
 import { waitForDomChange, fireEvent, render } from '@testing-library/react';
 import { ReaderContext, ReaderState } from 'components/reader/Reader';
+import { SnackbarProvider } from 'notistack';
 import { ReaderToolbar } from 'components/reader/ReaderToolbar';
 import { defaultRecipe } from 'lib/reader/recipes';
 import { MemoryRouter } from 'react-router-dom';
 import { Illuminsight } from 'types/illuminsight';
 import { testPub } from 'lib/test/data';
+import localForage from 'localforage';
 import * as React from 'react';
+import axios from 'axios';
 
 test('<ReaderToolbar>', async () => {
   // Wrap <ReaderToolbar>
@@ -21,21 +24,62 @@ test('<ReaderToolbar>', async () => {
     const [pub, setPub] = React.useState(testPub);
     state.pub = pub;
     return (
-      <MemoryRouter>
-        <ReaderContext.Provider value={state}>
-          <ReaderToolbar
-            onNavigate={setPub}
-            onInsight={() => 1}
-            history={history}
-          />
-        </ReaderContext.Provider>
-      </MemoryRouter>
+      <SnackbarProvider>
+        <MemoryRouter>
+          <ReaderContext.Provider value={state}>
+            <ReaderToolbar
+              onNavigate={setPub}
+              onInsight={() => 1}
+              history={history}
+            />
+          </ReaderContext.Provider>
+        </MemoryRouter>
+      </SnackbarProvider>
     );
   };
+
+  // Mock axios/localForage for auto recipe matching
+  const mockSetItem = ((localForage as any).setItem = jest.fn());
+  const mockGetItem = ((localForage as any).getItem = jest.fn());
+  const mockGet = ((axios as any).get = jest.fn());
+
+  // Mock loading saved recipe (none saved)
+  mockGetItem.mockResolvedValueOnce(null);
+
+  // Mock loading recipe list
+  mockGet.mockResolvedValueOnce({
+    data: [{ i: 'jane-austen', a: 'Jane Austen', b: 'Pride and Prejudice' }]
+  });
+
+  // Mock loading full recipe once matched to pub
+  const mockRecipe = { ...defaultRecipe };
+  mockGet.mockResolvedValueOnce({ data: defaultRecipe });
+
+  // Mock saving downloaded recipe
+  mockGetItem.mockResolvedValueOnce(undefined);
 
   // Render <ReaderToolbar> inside <ReaderToolbarConsumer>
   const { getByLabelText, getByTitle, getByText } = render(
     <ReaderToolbarConsumer />
+  );
+  await waitForDomChange();
+
+  // Validate a saved recipe was checked for
+  expect(mockGetItem).toHaveBeenCalledTimes(1);
+  expect(mockGetItem).toHaveBeenCalledWith(`pub-recipe-${testPub.id}`);
+
+  // Validate the recipe list was loaded
+  expect(mockGet).toHaveBeenCalledTimes(2);
+  expect(mockGet.mock.calls[0][0]).toEndWith('.index.min.json');
+
+  // Validate full recipe was downloaded after match
+  expect(mockGet.mock.calls[1][0]).toEndWith('jane-austen.min.json');
+
+  // Validate full recipe was saved
+  expect(mockSetItem).toHaveBeenCalledTimes(1);
+  expect(mockSetItem).toHaveBeenCalledWith(
+    `pub-recipe-${testPub.id}`,
+    mockRecipe
   );
 
   // Validate controls
