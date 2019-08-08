4e60e201fe7d4509af31a1e2739bf900e7057d13
MrXyfir <mr@xyfir.com>
2019-08-08 14:53:52 -0700

    Remove API_URL env

--- lib/app/api.ts
+++ lib/app/api.ts
@@ -1,6 +1,3 @@
 import axios from 'axios';
 
-export const api = axios.create({
-  baseURL: process.enve.API_URL,
-  withCredentials: true
-});
+export const api = axios.create({ withCredentials: true });
