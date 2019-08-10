7d3e240cf730a9216dfb67db857099897869a303
MrXyfir <mr@xyfir.com>
2019-08-09 18:32:30 -0700

    Update tests

--- __tests__/lib/import/convert.spec.ts
+++ __tests__/lib/import/convert.spec.ts
@@ -33,11 +33,9 @@ test('convert()', async () => {
     ...pub,
     authors: 'Charles Dickens',
     bookmark: { section: 0, element: 0 },
-    series: 'Test Series',
-    cover: 'res/0.jpg',
     name: 'A Tale of Two Cities',
     languages: ['en'],
-    sections: 54,
+    sections: 16,
     starred: false,
     tags: [],
     version: ASTPUB_VERSION,
@@ -47,12 +45,13 @@ test('convert()', async () => {
 
   expect(pub.toc).toBeArrayOfSize(50);
   expect(pub.toc.slice(0, 2)).toMatchObject([
-    { element: 'pgepubid00000', section: 1, title: 'A TALE OF TWO CITIES' },
-    { element: 0, section: 2, title: 'A STORY OF THE FRENCH REVOLUTION' }
+    { element: 'pgepubid00000', section: 0, title: 'A TALE OF TWO CITIES' },
+    {
+      element: 'pgepubid00001',
+      section: 0,
+      title: 'A STORY OF THE FRENCH REVOLUTION'
+    }
   ] as Illuminsight.Pub['toc']);
-
-  // Validate cover
-  expect(astpub.file('res/0.jpg')).not.toBeNull();
 
   // Validate AST
   const ast: Illuminsight.AST[] = JSON.parse(
