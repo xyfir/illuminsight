106dd1f9ef257ae34b4d877c0792ec0d904a14c2
MrXyfir <mr@xyfir.com>
2019-08-10 19:33:40 -0700

    Remove outdated setup file

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  setupFilesAfterEnv: [
-    '@testing-library/react/cleanup-after-each',
-    '<rootDir>/lib/test/setup.ts'
-  ],
+  setupFilesAfterEnv: ['<rootDir>/lib/test/setup.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
