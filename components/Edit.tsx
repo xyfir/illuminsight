e457f6ccebccd15580c1716e328c86d482acfe7c
MrXyfir <mr@xyfir.com>
2019-10-15 19:37:19 -0700

    Convert to hooks and fix eslint problems

--- components/Edit.tsx
+++ components/Edit.tsx
@@ -1,7 +1,7 @@
-import { WithSnackbarProps, withSnackbar } from 'notistack';
-import { RouteComponentProps } from 'react-router';
+import { useRouteMatch, useHistory } from 'react-router-dom';
 import { GeneralToolbar } from 'components/app/GeneralToolbar';
 import { Illuminsight } from 'types';
+import { useSnackbar } from 'notistack';
 import localForage from 'localforage';
 import * as React from 'react';
 import ISO6391 from 'iso-639-1';
@@ -10,20 +10,17 @@ import {
   InputAdornment,
   createStyles,
   IconButton,
-  WithStyles,
-  withStyles,
+  makeStyles,
+  Typography,
   TextField,
   Button,
-  Theme,
   Chip,
-  Typography,
 } from '@material-ui/core';
 import {
   BookmarkBorder as BookmarkIcon,
   DeleteForever as DeleteIcon,
-  // DateRange as PublishedIcon,
-  People as AuthorsIcon,
   GroupWork as SeriesIcon,
+  People as AuthorsIcon,
   Image as ImageIcon,
   Label as NameIcon,
   Home as PublisherIcon,
@@ -31,7 +28,7 @@ import {
   Add as AddIcon,
 } from '@material-ui/icons';
 
-const styles = (theme: Theme) =>
+const useStyles = makeStyles(() =>
   createStyles({
     coverContainer: {
       flexDirection: 'column',
@@ -71,105 +68,65 @@ const styles = (theme: Theme) =>
     root: {
       padding: '1em',
     },
-  });
+  }),
+);
 
-interface EditState {
-  language: Illuminsight.Pub['languages'][0];
-  cover?: string;
-  tags: Illuminsight.Tag[];
-  pub?: Illuminsight.Pub;
-  tag: string;
-}
-type EditProps = WithStyles<typeof styles> &
-  RouteComponentProps &
-  WithSnackbarProps;
+let zip: JSZip;
 
-class _Edit extends React.Component<EditProps, EditState> {
-  state: EditState = { language: '', tags: [], tag: '' };
-  zip?: JSZip;
+export function Edit(): JSX.Element | null {
+  const [language, setLanguage] = React.useState<
+    Illuminsight.Pub['languages'][0]
+  >('');
+  const { enqueueSnackbar } = useSnackbar();
+  const [cover, setCover] = React.useState<string | undefined>();
+  const [tags, setTags] = React.useState<Illuminsight.Tag[]>([]);
+  const [pub, setPub] = React.useState<Illuminsight.Pub | null>(null);
+  const [tag, setTag] = React.useState('');
+  const history = useHistory();
+  const classes = useStyles();
+  const match = useRouteMatch();
 
-  constructor(props: EditProps) {
-    super(props);
+  function onChange(prop: keyof Illuminsight.Pub, value: any): void {
+    if (pub) setPub({ ...pub, [prop]: value });
   }
 
-  async componentDidMount() {
-    const { enqueueSnackbar, history, match } = this.props;
-    const { pubId } = match.params as { pubId: number };
-
-    try {
-      // Load from localForage
-      const file: Blob = await localForage.getItem(`pub-${pubId}`);
-      if (file === null) throw 'Could not load data from storage';
-      const tags: Illuminsight.Tag[] = await localForage.getItem('tag-list');
-
-      // Parse zip file and meta
-      this.zip = await JSZip.loadAsync(file);
-      const pub: Illuminsight.Pub = JSON.parse(
-        await this.zip.file('meta.json').async('text'),
-      );
-
-      // Load cover and generate blob url
-      let cover;
-      if (pub.cover) {
-        cover = URL.createObjectURL(
-          await this.zip.file(pub.cover).async('blob'),
-        );
-      }
-
-      // Set state
-      this.setState({ pub, cover, tags });
-    } catch (err) {
-      // Notify user of error and send them back
-      console.error(err);
-      enqueueSnackbar(typeof err == 'string' ? err : 'Cannot read content');
-      history.replace('/');
-    }
-  }
-
-  componentWillUnmount() {
-    const { cover } = this.state;
-    if (cover) URL.revokeObjectURL(cover);
-  }
-
-  onResetBookmark() {
-    this.onChange('bookmark', {
+  function onResetBookmark(): void {
+    onChange('bookmark', {
       element: 0,
       section: 0,
     } as Illuminsight.Pub['bookmark']);
   }
 
-  onUploadCover(file: File) {
-    const { pub, cover } = this.state;
-    if (!pub || !this.zip) return;
+  function onUploadCover(file: File): void {
+    if (!pub || !zip) return;
 
     // Delete cover if not original
-    if (pub.cover && !/^res\/\d+\./.test(pub.cover)) this.zip.remove(pub.cover);
+    if (pub.cover && !/^res\/\d+\./.test(pub.cover)) zip.remove(pub.cover);
 
     // Update zip file
     pub.cover = `res/cover.${file.name.split('.').pop()}`;
-    this.zip.file(pub.cover, file);
-    this.zip.file('meta.json', JSON.stringify(pub));
+    zip.file(pub.cover, file);
+    zip.file('meta.json', JSON.stringify(pub));
 
     // Revoke old cover
     if (cover) URL.revokeObjectURL(cover);
 
     // Generate cover url
-    this.setState({ pub, cover: URL.createObjectURL(file) });
+    setPub(pub);
+    setCover(URL.createObjectURL(file));
   }
 
-  onAddLanguage() {
-    const { language, pub } = this.state;
+  function onAddLanguage(): void {
     const code = ISO6391.getCode(language);
     if (!code) return;
 
     // Add language to pub
     if (!pub!.languages.includes(code)) pub!.languages.push(code);
 
-    this.setState({ pub });
+    setPub(pub);
   }
 
-  onAddTag() {
-    const { pub, tags, tag } = this.state;
+  function onAddTag(): void {
     const name = tag.toLowerCase().replace(/\s+/g, '-');
 
     // Check if tag already exists
@@ -184,12 +141,22 @@ class _Edit extends React.Component<EditProps, EditState> {
     // Add tag to pub
     if (!pub!.tags.includes(_tag.id)) pub!.tags.push(_tag.id);
 
-    this.setState({ tags, tag: '' });
+    setTags(tags);
+    setTag('');
   }
 
-  async onDelete() {
-    const { enqueueSnackbar, history } = this.props;
-    const pub = this.state.pub!;
+  async function saveTags(pubs: Illuminsight.Pub[]): Promise<void> {
+    let _tags = tags.slice();
+    // Delete orphaned tags
+    for (const tag of _tags) {
+      if (pubs.findIndex((p) => p.tags.includes(tag.id)) == -1)
+        _tags = _tags.filter((t) => t.id != tag.id);
+    }
+    await localForage.setItem('tag-list', _tags);
+  }
+
+  async function onDelete(): Promise<void> {
+    if (!pub) return;
 
     // Remove pub from list
     let pubs: Illuminsight.Pub[] = await localForage.getItem('pub-list');
@@ -201,37 +168,30 @@ class _Edit extends React.Component<EditProps, EditState> {
     await localForage.removeItem(`pub-cover-${pub.id}`);
 
     // Update tags
-    await this.saveTags(pubs);
+    await saveTags(pubs);
 
     // Take us home and notify user
     history.replace('/');
-    // enqueueSnackbar(`${pub.name} was deleted`);
+    enqueueSnackbar(`${pub.name} was deleted`);
   }
 
-  onChange(prop: keyof Illuminsight.Pub, value: any) {
-    const pub = this.state.pub!;
-    this.setState({ pub: { ...pub, [prop]: value } });
-  }
-
-  async onSave() {
-    const { pub } = this.state;
-    if (!pub || !this.zip) return;
-    const { enqueueSnackbar } = this.props;
+  async function onSave(): Promise<void> {
+    if (!pub || !zip) return;
 
     // Update meta.json
-    this.zip.file('meta.json', JSON.stringify(pub));
+    zip.file('meta.json', JSON.stringify(pub));
 
     // Save file
     await localForage.setItem(
       `pub-${pub.id}`,
-      await this.zip.generateAsync({ type: 'blob' }),
+      await zip.generateAsync({ type: 'blob' }),
     );
 
     // Extract cover if available and save copy outside of zip
     if (pub.cover) {
       await localForage.setItem(
         `pub-cover-${pub.id}`,
-        await this.zip.file(pub.cover).async('blob'),
+        await zip.file(pub.cover).async('blob'),
       );
     }
 
@@ -242,222 +202,233 @@ class _Edit extends React.Component<EditProps, EditState> {
     await localForage.setItem('pub-list', pubs);
 
     // Update tags and notify user
-    await this.saveTags(pubs);
-    // enqueueSnackbar(`${pub.name} was updated`);
+    await saveTags(pubs);
+    enqueueSnackbar(`${pub.name} was updated`);
   }
 
-  async saveTags(pubs: Illuminsight.Pub[]) {
-    // Delete orphaned tags
-    let tags = this.state.tags;
-    for (const tag of tags) {
-      if (pubs.findIndex((p) => p.tags.includes(tag.id)) == -1)
-        tags = tags.filter((t) => t.id != tag.id);
+  async function onMount(): Promise<void> {
+    const { pubId } = match!.params as { pubId: number };
+
+    try {
+      // Load from localForage
+      const file: Blob = await localForage.getItem(`pub-${pubId}`);
+      if (file === null) throw 'Could not load data from storage';
+      const tags: Illuminsight.Tag[] = await localForage.getItem('tag-list');
+
+      // Parse zip file and meta
+      zip = await JSZip.loadAsync(file);
+      const pub: Illuminsight.Pub = JSON.parse(
+        await zip.file('meta.json').async('text'),
+      );
+
+      // Load cover and generate blob url
+      let cover;
+      if (pub.cover) {
+        cover = URL.createObjectURL(await zip.file(pub.cover).async('blob'));
+      }
+
+      // Set state
+      setPub(pub);
+      setTags(tags);
+      setCover(cover);
+    } catch (err) {
+      // Notify user of error and send them back
+      console.error(err);
+      enqueueSnackbar(typeof err == 'string' ? err : 'Cannot read content');
+      history.replace('/');
     }
-    await localForage.setItem('tag-list', tags);
   }
 
-  render() {
-    const { language, cover, tags, tag, pub } = this.state;
-    const { classes } = this.props;
-    if (!pub) return null;
+  // on mount
+  React.useEffect(() => {
+    onMount();
+    return (): void => {
+      if (cover) URL.revokeObjectURL(cover);
+      zip = undefined;
+    };
+  }, []);
 
-    return (
-      <form className={classes.root}>
-        <GeneralToolbar />
+  if (!pub) return null;
 
+  return (
+    <form className={classes.root}>
+      <GeneralToolbar />
+
+      <TextField
+        id="name"
+        label="Name"
+        value={pub.name}
+        margin="normal"
+        variant="outlined"
+        onChange={(e): void => onChange('name', e.target.value)}
+        fullWidth
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <NameIcon />
+            </InputAdornment>
+          ),
+        }}
+        placeholder="A Tale of Two Cities"
+      />
+
+      <TextField
+        id="authors"
+        label="Author(s)"
+        value={pub.authors}
+        margin="normal"
+        variant="outlined"
+        onChange={(e): void => onChange('authors', e.target.value)}
+        fullWidth
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AuthorsIcon />
+            </InputAdornment>
+          ),
+        }}
+        placeholder="Ernest Hemingway"
+      />
+
+      <TextField
+        id="series"
+        label="Series"
+        value={pub.series}
+        margin="normal"
+        variant="outlined"
+        onChange={(e): void => onChange('series', e.target.value)}
+        fullWidth
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SeriesIcon />
+            </InputAdornment>
+          ),
+        }}
+        placeholder="The Lord of the Rings"
+      />
+
+      <TextField
+        id="publisher"
+        label="Publisher"
+        value={pub.publisher}
+        margin="normal"
+        variant="outlined"
+        onChange={(e): void => onChange('publisher', e.target.value)}
+        fullWidth
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <PublisherIcon />
+            </InputAdornment>
+          ),
+        }}
+        placeholder="Penguin Random House"
+      />
+
+      <div className={classes.coverContainer}>
+        {cover ? <img src={cover} className={classes.cover} /> : null}
+        <input
+          id="cover-input"
+          type="file"
+          multiple
+          onChange={(e): void => onUploadCover((e.target.files as FileList)[0])}
+          className={classes.coverInput}
+        />
+        <label htmlFor="cover-input">
+          <Button variant="text" component="span">
+            <ImageIcon />
+            Set Cover
+          </Button>
+        </label>
+      </div>
+
+      <div className={classes.add}>
         <TextField
-          id="name"
-          label="Name"
-          value={pub.name}
+          id="tag"
+          label="Tag"
+          value={tag}
           margin="normal"
           variant="outlined"
-          onChange={(e) => this.onChange('name', e.target.value)}
+          onChange={(e): void => setTag(e.target.value)}
           fullWidth
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <NameIcon />
-              </InputAdornment>
-            ),
-          }}
-          placeholder="A Tale of Two Cities"
+          placeholder="#tag"
         />
+        <IconButton aria-label="Add tag" onClick={onAddTag} color="primary">
+          <AddIcon />
+        </IconButton>
+      </div>
 
-        <TextField
-          id="authors"
-          label="Author(s)"
-          value={pub.authors}
-          margin="normal"
-          variant="outlined"
-          onChange={(e) => this.onChange('authors', e.target.value)}
-          fullWidth
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <AuthorsIcon />
-              </InputAdornment>
-            ),
-          }}
-          placeholder="Ernest Hemingway"
-        />
-
-        <TextField
-          id="series"
-          label="Series"
-          value={pub.series}
-          margin="normal"
-          variant="outlined"
-          onChange={(e) => this.onChange('series', e.target.value)}
-          fullWidth
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SeriesIcon />
-              </InputAdornment>
-            ),
-          }}
-          placeholder="The Lord of the Rings"
-        />
-
-        <TextField
-          id="publisher"
-          label="Publisher"
-          value={pub.publisher}
-          margin="normal"
-          variant="outlined"
-          onChange={(e) => this.onChange('publisher', e.target.value)}
-          fullWidth
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <PublisherIcon />
-              </InputAdornment>
-            ),
-          }}
-          placeholder="Penguin Random House"
-        />
-
-        <div className={classes.coverContainer}>
-          {cover ? <img src={cover} className={classes.cover} /> : null}
-          <input
-            id="cover-input"
-            type="file"
-            multiple
-            onChange={(e) =>
-              this.onUploadCover((e.target.files as FileList)[0])
+      <div>
+        {pub.tags.map((tag) => (
+          <Chip
+            className={classes.chip}
+            onDelete={(): void =>
+              onChange('tags', pub.tags.filter((t) => t != tag))
             }
-            className={classes.coverInput}
+            label={`#${tags.find((t) => t.id == tag)!.name}`}
+            key={tag}
           />
-          <label htmlFor="cover-input">
-            <Button variant="text" component="span">
-              <ImageIcon />
-              Set Cover
-            </Button>
-          </label>
-        </div>
+        ))}
+      </div>
 
-        <div className={classes.add}>
-          <TextField
-            id="tag"
-            label="Tag"
-            value={tag}
-            margin="normal"
-            variant="outlined"
-            onChange={(e) => this.setState({ tag: e.target.value })}
-            fullWidth
-            placeholder="#tag"
+      <div className={classes.add}>
+        <TextField
+          id="languages"
+          label="Language"
+          value={language}
+          margin="normal"
+          variant="outlined"
+          onChange={(e): void => setLanguage(e.target.value)}
+          fullWidth
+          placeholder="Language"
+        />
+        <IconButton
+          aria-label="Add language"
+          onClick={onAddLanguage}
+          color="primary"
+        >
+          <AddIcon />
+        </IconButton>
+      </div>
+
+      <div>
+        {pub.languages.map((lang) => (
+          <Chip
+            className={classes.chip}
+            onDelete={(): void =>
+              onChange('languages', pub.languages.filter((l) => l != lang))
+            }
+            label={ISO6391.getName(lang)}
+            key={lang}
           />
-          <IconButton
-            aria-label="Add tag"
-            onClick={() => this.onAddTag()}
-            color="primary"
-          >
-            <AddIcon />
-          </IconButton>
-        </div>
+        ))}
+      </div>
 
-        <div>
-          {pub.tags.map((tag) => (
-            <Chip
-              className={classes.chip}
-              onDelete={() =>
-                this.onChange('tags', pub.tags.filter((t) => t != tag))
-              }
-              label={`#${tags.find((t) => t.id == tag)!.name}`}
-            />
-          ))}
-        </div>
-
-        <div className={classes.add}>
-          <TextField
-            id="languages"
-            label="Language"
-            value={language}
-            margin="normal"
-            variant="outlined"
-            onChange={(e) => this.setState({ language: e.target.value })}
-            fullWidth
-            placeholder="Language"
-          />
-          <IconButton
-            aria-label="Add language"
-            onClick={() => this.onAddLanguage()}
-            color="primary"
-          >
-            <AddIcon />
-          </IconButton>
-        </div>
-
-        <div>
-          {pub.languages.map((lang) => (
-            <Chip
-              className={classes.chip}
-              onDelete={() =>
-                this.onChange(
-                  'languages',
-                  pub.languages.filter((l) => l != lang),
-                )
-              }
-              label={ISO6391.getName(lang)}
-            />
-          ))}
-        </div>
-
-        {pub.bookmark.section != 0 || pub.bookmark.element != 0 ? (
-          <div className={classes.bookmark}>
-            <Button onClick={() => this.onResetBookmark()} variant="text">
-              <BookmarkIcon />
-              Remove
-            </Button>
-            <Typography>
-              bookmark at section <strong>#{pub.bookmark.section + 1}</strong>,
-              element <strong>#{pub.bookmark.element}</strong>.
-            </Typography>
-          </div>
-        ) : null}
-
-        <div className={classes.buttons}>
-          <Button
-            onClick={() => this.onSave()}
-            variant="contained"
-            color="primary"
-          >
-            <SaveIcon />
-            Update
+      {pub.bookmark.section != 0 || pub.bookmark.element != 0 ? (
+        <div className={classes.bookmark}>
+          <Button onClick={onResetBookmark} variant="text">
+            <BookmarkIcon />
+            Remove
           </Button>
-
-          <Button
-            onClick={() => this.onDelete()}
-            variant="contained"
-            color="secondary"
-          >
-            <DeleteIcon />
-            Delete
-          </Button>
+          <Typography>
+            bookmark at section <strong>#{pub.bookmark.section + 1}</strong>,
+            element <strong>#{pub.bookmark.element}</strong>.
+          </Typography>
         </div>
-      </form>
-    );
-  }
+      ) : null}
+
+      <div className={classes.buttons}>
+        <Button onClick={onSave} variant="contained" color="primary">
+          <SaveIcon />
+          Update
+        </Button>
+
+        <Button onClick={onDelete} variant="contained" color="secondary">
+          <DeleteIcon />
+          Delete
+        </Button>
+      </div>
+    </form>
+  );
 }
-
-export const Edit = withStyles(styles)(withSnackbar(_Edit));
