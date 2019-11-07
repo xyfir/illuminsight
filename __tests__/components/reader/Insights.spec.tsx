8e120be12aa976c65ddb63cacba82e007b73d161
MrXyfir <mr@xyfir.com>
2019-11-06 19:29:35 -0800

    Update tests

--- __tests__/components/reader/Insights.spec.tsx
+++ __tests__/components/reader/Insights.spec.tsx
@@ -1,113 +1,79 @@
-// import { testDefinitions, testWikitext, testPub } from 'lib/test/data';
-// import { fireEvent, render, wait } from '@testing-library/react';
-// import { defaultRecipe } from 'lib/reader/recipes';
-// import { createStore } from 'redux';
-// import { Insights } from 'components/reader/Insights';
-// import { Provider } from 'react-redux';
-// import { reducer } from 'store/reducers';
-// import * as React from 'react';
-// import axios from 'axios';
-// import wtf from 'wtf_wikipedia';
+import { testDefinitions, testWikitext } from 'lib/test/data';
+import { fireEvent, render } from '@testing-library/react';
+import { defaultRecipe } from 'lib/reader/recipes';
+import { createStore } from 'redux';
+import { Insights } from 'components/reader/Insights';
+import { Provider } from 'react-redux';
+import { reducer } from 'store/reducers';
+import * as React from 'react';
+import wtf from 'wtf_wikipedia';
 
 test('<Insights>', () => {
   expect(true).toBe(true);
 
-  // // Mocks
-  // const mockFetch = ((wtf as any).fetch = jest.fn());
-  // const mockOpen = ((window as any).open = jest.fn());
-  // const mockGet = ((axios as any).get = jest.fn());
+  const store = createStore(reducer, {
+    insights: {
+      definitions: testDefinitions,
+      searches: [
+        {
+          name: 'Google',
+          url: 'https://www.google.com/search?q=Blood%20Meridian',
+        },
+      ],
+      wikis: [
+        {
+          recipe: defaultRecipe.wikis[0],
+          doc: wtf(testWikitext),
+        },
+      ],
+      text: 'Blood Meridian',
+    },
+    recipe: defaultRecipe,
+    ast: [],
+  });
+  const { getByLabelText, getAllByText, getByText } = render(
+    <Provider store={store}>
+      <Insights />
+    </Provider>,
+  );
 
-  // // Mock getting definitions / wiki
-  // mockGet.mockResolvedValueOnce(null);
-  // mockGet.mockResolvedValueOnce({ data: testDefinitions });
-  // mockFetch.mockResolvedValueOnce(null);
-  // mockFetch.mockResolvedValueOnce(wtf(testWikitext));
+  // Expect insight to have opened definitions
+  getAllByText('noun');
 
-  // const store = createStore(reducer, {
-  //   insights: {
-  //     definitions: testDefinitions,
-  //     searches: [
-  //       {
-  //         name: 'Google',
-  //         url: 'https://www.google.com/search?q=Blood%20Meridian',
-  //       },
-  //     ],
-  //     wikis: [],
-  //     text: 'Blood Meridian',
-  //   },
-  //   recipe: defaultRecipe,
-  //   pub: testPub,
-  //   ast: [],
-  // });
-  // const { getByLabelText, getAllByText, getByText } = render(
-  //   <Provider store={store}>
-  //     <Insights />
-  //   </Provider>,
-  // );
+  // Click "Google" insight
+  const mockOpen = ((window as any).open = jest.fn());
+  fireEvent.click(getByText('Google'));
 
-  // // Click "Cormac McCarthy" insight
-  // fireEvent.click(getByText('Cormac McCarthy'));
+  // Expect "Search" insight to have opened Google search
+  expect(mockOpen).toHaveBeenCalledTimes(1);
+  expect(mockOpen).toHaveBeenCalledWith(
+    'https://www.google.com/search?q=Blood%20Meridian',
+  );
 
-  // // Expect "Cormac McCarthy" insight to have opened search
-  // expect(mockOpen).toHaveBeenCalledTimes(1);
-  // expect(mockOpen).toHaveBeenCalledWith(
-  //   'https://www.google.com/search?q=Cormac%20McCarthy',
-  // );
+  // Click "Wikipedia" insight
+  fireEvent.click(getByText('Wikipedia'));
 
-  // // Click "Blood Meridian" insight
-  // fireEvent.click(getByText('Blood Meridian'));
+  // Expect definitions to have closed
+  expect(() => getAllByText('noun')).toThrow();
 
-  // // Expect "Blood Meridian" insight to have opened definitions
-  // getAllByText('noun');
+  // Expect "Wikipedia" insight to have opened wiki article
+  getByText('novel by American author', { exact: false });
 
-  // // Click "Blood Meridian" insight again to definitions
-  // fireEvent.click(getAllByText('Blood Meridian')[0]);
+  // Click "Definiton" insight
+  fireEvent.click(getByText('Definition'));
 
-  // // Expect definitions to have closed
-  // expect(() => getAllByText('noun')).toThrow();
+  // Validate definition has loaded
+  getAllByText('noun');
 
-  // // Click secondary action to view all insights of text
-  // fireEvent.click(getByLabelText('View all insights for "Blood Meridian"'));
+  // Validate panel is not expanded
+  getByLabelText('Expand insights panel');
+  expect(() => getByLabelText('Restore insights panel size')).toThrow();
 
-  // // Wait for all insights to load
-  // await wait(() => expect(mockFetch).toHaveBeenCalledTimes(2));
-  // expect(mockGet).toHaveBeenCalledTimes(2);
+  // Expand panel
+  fireEvent.click(getByLabelText('Expand insights panel'));
+  getByLabelText('Restore insights panel size');
 
-  // // Expect other insights to be gone
-  // expect(() => getByText('Cormac McCarthy')).toThrow();
-
-  // // Click "Google" insight
-  // fireEvent.click(getByText('Google'));
-
-  // // Expect "Search" insight to have opened Google search
-  // expect(mockOpen).toHaveBeenCalledTimes(2);
-  // expect(mockOpen).toHaveBeenCalledWith(
-  //   'https://www.google.com/search?q=Blood%20Meridian',
-  // );
-
-  // // Click "Wikipedia" insight
-  // fireEvent.click(getByText('Wikipedia'));
-
-  // // Expect "Wikipedia" insight to have opened wiki article
-  // getByText('novel by American author', { exact: false });
-
-  // // Expect only top-level insights to be rendered
-  // // Expect wiki to remain rendered
-  // expect(getAllByText('Cormac McCarthy')).toBeArrayOfSize(2);
-  // expect(() => getByText('Wikipedia')).toThrow();
-
-  // // Click secondary action to view all insights of text
-  // fireEvent.click(getByLabelText('View all insights for "Blood Meridian"'));
-
-  // // Click back to previous insights
-  // fireEvent.click(getByLabelText('Back to previous insights'));
-
-  // // Click secondary action to view all insights of text
-  // fireEvent.click(getByLabelText('View all insights for "Blood Meridian"'));
-
-  // // Click "Definiton" insight
-  // fireEvent.click(getByText('Definition'));
-
-  // // Validate definition has loaded
-  // getAllByText('noun');
+  // Close panel
+  fireEvent.click(getByLabelText('Close insights panel'));
+  expect(() => getByLabelText('Close insights panel')).toThrow();
 });
